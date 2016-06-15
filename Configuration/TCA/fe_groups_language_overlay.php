52c99bec664cf9299469a4c68a75f86a2d8de1ee
Franz Holzinger <holzinger@ttproducts.de>
2016-06-15 12:53:55 +0200

    add the Configuration folder which is needed since TYPO3 6.2

---
+++ Configuration/TCA/fe_groups_language_overlay.php
@@ -0,0 +1,66 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+
+$result = array(
+	'ctrl' => array(
+		'title' => 'LLL:EXT:' . AGENCY_EXT . '/locallang_db.xml:fe_groups_language_overlay',
+		'label' => 'title',
+		'default_sortby' => 'ORDER BY fe_groups_uid',
+		'sortby' => 'sorting',
+		'delete' => 'deleted',
+		'enablecolumns' => array(
+			'disabled' => 'hidden'
+		),
+		'iconfile' => 'gfx/i/fe_groups.gif',
+	),
+	'interface' => array (
+		'showRecordFieldList' => 'hidden,fe_group,title'
+	),
+	'columns' => array(
+		'hidden' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.hidden',
+			'config' => array(
+				'type' => 'check',
+				'default' => '0'
+			)
+		),
+		'fe_group' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:cms/locallang_tca.xml:fe_groups',
+			'config' => array(
+				'type' => 'select',
+				'foreign_table' => 'fe_groups'
+			)
+		),
+		'sys_language_uid' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.language',
+			'config' => array(
+				'type' => 'select',
+				'foreign_table' => 'sys_language',
+				'foreign_table_where' => 'ORDER BY sys_language.title'
+			)
+		),
+		'title' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:' . AGENCY_EXT . '/locallang_db.xml:fe_groups_language_overlay.title',
+			'config' => array(
+				'type' => 'input',
+				'size' => '50',
+				'max' => '70',
+				'eval' => 'trim,required',
+			)
+		),
+	),
+	'types' => array(
+		'0' => array( 'showitem' => 'hidden;;;;1-1-1, fe_group, sys_language_uid, title')
+	)
+);
+
+
+return $result;
+
---
+++ Configuration/TCA/sys_agency_fe_users_limit_fe_groups.php
@@ -0,0 +1,113 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+
+
+$result = array(
+	'ctrl' => array (
+		'title' => 'LLL:EXT:' . AGENCY_EXT . '/locallang_db.xml:sys_agency_fe_users_limit_fe_groups',
+		'label' => 'codes',
+		'default_sortby' => 'ORDER BY codes',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'delete' => 'deleted',
+		'enablecolumns' => array (
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		),
+		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath(AGENCY_EXT) . 'ext_icon.gif',
+	),
+	'interface' => array (
+		'showRecordFieldList' => 'hidden,starttime,endtime,codes,status'
+	),
+	'columns' => array (
+		'hidden' => array (
+			'exclude' => 1,
+			'label' => 'LLL:EXT:lang/locallang_general.php:LGL.hidden',
+			'config' => array (
+				'type' => 'check'
+			)
+		),
+		'tstamp' => array (
+			'exclude' => 1,
+			'label' => 'LLL:EXT:' . AGENCY_EXT . '/locallang_db.xml:tstamp',
+			'config' => array (
+				'type' => 'input',
+				'size' => '8',
+				'max' => '20',
+				'eval' => 'date',
+				'default' => '0'
+			)
+		),
+		'crdate' => array (
+			'exclude' => 1,
+			'label' => 'LLL:EXT:' . AGENCY_EXT . '/locallang_db.xml:crdate',
+			'config' => array (
+				'type' => 'input',
+				'size' => '8',
+				'max' => '20',
+				'eval' => 'date',
+				'default' => '0'
+			)
+		),
+		'starttime' => array (
+			'exclude' => 1,
+			'label' => 'LLL:EXT:lang/locallang_general.php:LGL.starttime',
+			'config' => array (
+				'type' => 'input',
+				'size' => '8',
+				'max' => '20',
+				'eval' => 'date',
+				'checkbox' => '0',
+				'default' => '0'
+			)
+		),
+		'endtime' => array (
+			'exclude' => 1,
+			'label' => 'LLL:EXT:lang/locallang_general.php:LGL.endtime',
+			'config' => array (
+				'type' => 'input',
+				'size' => '8',
+				'max' => '20',
+				'eval' => 'date',
+				'checkbox' => '0',
+				'default' => '0',
+				'range' => array (
+					'upper' => mktime(0, 0, 0, 12, 31, $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][AGENCY_EXT]['endtimeYear']),
+					'lower' => mktime(0, 0, 0, date('n') - 1, date('d'), date('Y'))
+				)
+			)
+		),
+		'codes' => array (
+			'exclude' => 0,
+			'label' => 'LLL:EXT:' . AGENCY_EXT . '/locallang_db.xml:sys_agency_fe_users_limit_fe_groups.codes',
+			'config' => array (
+				'type' => 'text',
+				'cols' => '48',
+				'rows' => '5'
+			)
+		),
+		'status' => array (
+			'exclude' => 1,
+			'label' => 'LLL:EXT:' . AGENCY_EXT . '/locallang_db.xml:sys_agency_fe_users_limit_fe_groups.status',
+			'config' => array (
+				'type' => 'input',
+				'size' => '10',
+				'eval' => 'int',
+				'default' => '0',
+			)
+		),
+	),
+	'types' => array(
+		'0' => array( 'showitem' => 'hidden;;;;1-1-1, codes, status, starttime, endtime')
+	)
+);
+
+
+
+return $result;
+
