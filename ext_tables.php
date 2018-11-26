c886b13936c4423a6b0b9befc0b42d6d980cf170
Franz Holzinger <holzinger@ttproducts.de>
2018-11-26 16:06:34 +0100

    * Extract file tt_content.php from ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,51 +1,20 @@
 <?php
-if (!defined ('TYPO3_MODE')) {
-    die ('Access denied.');
-}
+defined('TYPO3_MODE') or die('Access denied.');
 
 $emClass = '\\TYPO3\\CMS\\Core\\Utility\\ExtensionManagementUtility';
-
-if (
-    class_exists($emClass) &&
-    method_exists($emClass, 'extPath')
-) {
-    // nothing
-} else {
-    $emClass = 't3lib_extMgm';
-}
-
-$divClass = '\\TYPO3\\CMS\\Core\\Utility\\GeneralUtility';
-
-if (
-    class_exists($divClass)
-) {
-    // nothing
-} else {
-    $divClass = 't3lib_div';
-}
-
-if (!isset($_EXTKEY)) {
-    $_EXTKEY = AGENCY_EXT;
-}
 
 if (
     TYPO3_MODE == 'BE' &&
     !$loadTcaAdditions
 ) {
-    call_user_func($emClass . '::addStaticFile', $_EXTKEY, 'Configuration/TypoScript/PluginSetup/', 'Agency Registration');
-
-    $listType = $_EXTKEY . '';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$listType] = 'layout,select_key';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$listType] = 'pi_flexform';
-    call_user_func($emClass . '::addPiFlexFormValue', $listType, 'FILE:EXT:' . $_EXTKEY . '/pi/flexform_ds_pi.xml');
-    call_user_func($emClass . '::addPlugin', array('LLL:EXT:' . $_EXTKEY . '/locallang_db.xml:tt_content.list_type', $listType), 'list_type');
+    call_user_func($emClass . '::addStaticFile', AGENCY_EXT, 'Configuration/TypoScript/PluginSetup/', 'Agency Registration');
 }
 
 call_user_func($emClass . '::allowTableOnStandardPages', 'fe_groups_language_overlay');
 call_user_func($emClass . '::addToInsertRecords', 'fe_groups_language_overlay');
 
 if ( // Direct Mail tables exist but Direct Mail shall not be used
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['enableDirectMail'] &&
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][AGENCY_EXT]['enableDirectMail'] &&
     !call_user_func($emClass . '::isLoaded', 'direct_mail')
 ) {
     call_user_func($emClass . '::allowTableOnStandardPages', 'sys_dmail_category');
