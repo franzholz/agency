68e339e62131b63b114c6bbb0db6b66c33b4f169
Franz Holzinger <franz.holzinger@festwein.de>
2023-06-03 20:00:22 +0200

    TYPO3_MODE => TYPO3

--- class.tx_agency.php
+++ class.tx_agency.php
@@ -50,7 +50,3 @@ class tx_agency {
     }
 }
 
-if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/' . AGENCY_EXT . '/class.tx_agency.php']) {
-    include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/' . AGENCY_EXT . '/class.tx_agency.php']);
-}
-
