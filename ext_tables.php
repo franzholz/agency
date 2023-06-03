4208bdf30676ddcfc01cf5cb59c6f831412506b8
Franz Holzinger <franz.holzinger@festwein.de>
2023-06-03 18:06:14 +0200

    move ExtensionManagementUtility::addToInsertRecords into Overrides TCA

--- ext_tables.php
+++ ext_tables.php
@@ -4,13 +4,11 @@ defined('TYPO3_MODE') || die('Access denied.');
 call_user_func(function () {
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages( 'fe_groups_language_overlay');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords( 'fe_groups_language_overlay');
 
     if ( // Direct Mail tables exist but Direct Mail shall not be used
         $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][AGENCY_EXT]['enableDirectMail'] &&
         !\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('direct_mail')
     ) {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages( 'sys_dmail_category');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords('sys_dmail_category');
     }
 });
