7c0a263448e8285700f546acdf6e691f48d3ee24
Franz Holzinger <franz.holzinger@festwein.de>
2025-08-04 18:44:48 +0200

    remove list_type and setup outside of Set

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,19 +2,25 @@
 
 defined('TYPO3') || die('Access denied.');
 
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 use JambageCom\Agency\Constants\Extension;
+use JambageCom\Agency\Hooks\RegistrationProcessHooks;
+use JambageCom\Agency\Hooks\StatusProvider;
+use JambageCom\Agency\Upgrades\PluginListTypeToCTypeUpdate;
+
 
 call_user_func(function ($extensionKey): void {
     $languageSubpath = '/Resources/Private/Language/';
-    $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+    $extensionConfiguration = GeneralUtility::makeInstance(
+        ExtensionConfiguration::class
     )->get($extensionKey);
 
     if (!defined('STATIC_INFO_TABLES_EXT')) {
         define('STATIC_INFO_TABLES_EXT', 'static_info_tables');
     }
-
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($extensionKey, '', '', 'list_type', 0);
 
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extensionKey]['uploadfolder'] = $extensionConfiguration['uploadFolder'] ?: 'uploads/tx_agency';
 
@@ -26,14 +32,14 @@ call_user_func(function ($extensionKey): void {
 
     /* Example of configuration of hooks */
     // $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extensionKey]['confirmRegistrationClass'][] = \MyWebsiteCom\MyExtension\Hooks\Handler::class;
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extensionKey]['registrationProcess'][] = \JambageCom\Agency\Hooks\RegistrationProcessHooks::class;
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extensionKey]['registrationProcess'][] = RegistrationProcessHooks::class;
 
     $EM_CONF = [];
     $_EXTKEY = $extensionKey;
 
     // Take note of conflicting extensions
     // Save extension version and constraints
-    include \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($extensionKey) . 'ext_emconf.php';
+    include ExtensionManagementUtility::extPath($extensionKey) . 'ext_emconf.php';
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extensionKey]['version'] = $EM_CONF[$extensionKey]['version'];
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extensionKey]['constraints'] = $EM_CONF[$extensionKey]['constraints'];
     unset($EM_CONF);
@@ -53,6 +59,8 @@ call_user_func(function ($extensionKey): void {
     ];
 
     // Register Status Report Hook
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['Agency Registration'][] = \JambageCom\Agency\Hooks\StatusProvider::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['Agency Registration'][] = StatusProvider::class;
 
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['agencyPluginListTypeToCTypeUpdate']
+        = PluginListTypeToCTypeUpdate::class;
 }, Extension::KEY);
